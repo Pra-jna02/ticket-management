8dcf40806c2ed7ea068642b9b2c81fabbdbf2947
Prajna Nayak <prajna.nayak1@mphasis.com>
2026-09-22 16:16:16 +0530

    Implemented user CRUD APIs

--- src/main/java/com/ticket/management/controller/UserController.java
+++ src/main/java/com/ticket/management/controller/UserController.java
@@ -1,4 +1,49 @@
 package com.ticket.management.controller;
 
+import com.ticket.management.dto.UserRequestDto;
+import com.ticket.management.dto.UserResponseDto;
+import com.ticket.management.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public UserResponseDto createUser(@Valid @RequestBody UserRequestDto request){
+
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAllUsers(){
+
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto getUserById(@PathVariable Long id){
+
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public UserResponseDto updateUser(@PathVariable Long id,
+                                      @Valid @RequestBody UserRequestDto request){
+
+        return userService.updateUser(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+
+        userService.deleteUser(id);
+    }
 }
